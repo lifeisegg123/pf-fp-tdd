5ed293d614411e8232176f617889cb1a313e44a6
lifeisegg123 <leejj2002@naver.com>
2022-06-13 22:55:07 +0900

    chore: add fx/ts & remove fp functions

--- src/types/promise.ts
+++ src/types/promise.ts
@@ -1,3 +1,5 @@
 export type MaybePromise<T> = Promise<T> | T;
 
 export type InferPromise<T> = T extends Promise<infer U> ? U : T;
+
+export type ReturnPromise<T, R> = T extends Promise<any> ? Promise<R> : R;
